6114f190774a37d636f39ad93583951edd15c290
Nemo <2402214215@qq.com>
2022-08-03 01:00:23 +0800

    modified

--- Codes/PART0/Chapter1/1.21/main.cpp
+++ Codes/PART0/Chapter1/1.21/main.cpp
@@ -4,12 +4,7 @@ int main()
 {
     Sales_item item1,item2;
     std::cin >> item1 >> item2;
-    if(compareIsbn(item1, item2)){
-        std::cout << item1+item2 << std::endl;
-        return 0;
-    }else{
-        std::cerr << "Data must refer to same ISBN" << std::endl;
-        return -1;
-    }
+    std::cout << item1 + item2 << std::endl;
+    return 0;
 }
 
--- Codes/PART0/Chapter1/1.23/main.cpp
+++ Codes/PART0/Chapter1/1.23/main.cpp
@@ -8,7 +8,7 @@ int main()
     std::cout << "Please enter a number of sales records:" << std::endl;
     if(std::cin >> book1){
         while(std::cin >> book2){
-            if(compareIsbn(book1, book2)){
+            if(book1.isbn() == book2.isbn()){
                 ++cnt;
             }else{
                 std::cout << book1.isbn() << " has " << cnt << " record(s)." << std::endl;
