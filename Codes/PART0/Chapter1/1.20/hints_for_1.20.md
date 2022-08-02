3092e824d93c87b52cf8e4bff377ba0355cf6540
Niki <1462533661@qq.com>
2022-08-02 23:51:38 +0800

    1.5 after merge

--- Codes/PART0/Chapter1/1.20/hints_for_1.20.md
+++ Codes/PART0/Chapter1/1.20/hints_for_1.20.md
@@ -1,4 +1,4 @@
 # hints for 1.20
 
-- 程序使用文件重定向，读入input.txt，输出output.txt
+- 程序使用文件重定向，读入infile.txt，输出outfile.txt
 - 1.21~1.25同理
--- Codes/PART0/Chapter1/1.23/main.cpp
+++ Codes/PART0/Chapter1/1.23/main.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 #include "../Sales_item.h"
 
-int main(){
-    Sales_item itemtmp, lastitem;
-    int count;
-    if(std::cin >> itemtmp){
-        lastitem = itemtmp;
-        count = 1;
-        while(std::cin >> itemtmp){
-            if(itemtmp.isbn() == lastitem.isbn())
-                count++;
-            else{
-                std::cout << lastitem.isbn() << " occurs " << count << " times." << std::endl;
-                lastitem = itemtmp;
-                count = 1;
+int main()
+{
+    Sales_item book1, book2;
+    int cnt = 1;
+    std::cout << "Please enter a number of sales records:" << std::endl;
+    if(std::cin >> book1){
+        while(std::cin >> book2){
+            if(compareIsbn(book1, book2)){
+                ++cnt;
+            }else{
+                std::cout << book1.isbn() << " has " << cnt << " record(s)." << std::endl;
+                book1 = book2;
+                cnt = 1;
             }
         }
-        std::cout << lastitem.isbn() << " occurs " << count << " times." << std::endl;
-        return 0;
-    }
-    else{
-        std::cerr << "Please input at least one legal item." << std::endl;
+        std::cout << book1.isbn() << " has " << cnt << " record(s)." << std::endl;
+    }else{
+        std::cout << "Incorrect input." << std::endl;
         return -1;
     }
+
+    return 0;
 }
